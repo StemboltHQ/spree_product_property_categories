61687e960e6d1f862e0a6112205c8b84bf1d0bac
Richard Wilson <richard@freerunningtech.com>
2014-05-28 17:07:56 -0700

    Wrap controller logic in a transaction

--- app/controllers/spree/api/property_categories_controller.rb
+++ app/controllers/spree/api/property_categories_controller.rb
@@ -5,19 +5,21 @@ module Spree
         authorize! :update, Spree::PropertyCategory
 
         if params[:product_categories] && product = Spree::Product.find_by(slug: params[:product_id])
-          product.product_properties.destroy_all
-          params[:product_categories].values.each do |category|
-            next unless category[:properties]
-            category[:properties].values.each do |pp|
-              next if pp[:key].blank?
-              Spree::ProductProperty.create!(
-                product: product,
-                category_name: category[:name].blank? ?
-                Spree.t(:default_category_name) :
-                category[:name],
-                property_name: pp[:key],
-                value: pp[:value]
-              )
+          Spree::PropertyCategory.transaction do
+            product.product_properties.destroy_all
+            params[:product_categories].values.each do |category|
+              next unless category[:properties]
+              category[:properties].values.each do |pp|
+                next if pp[:key].blank?
+                Spree::ProductProperty.create!(
+                  product: product,
+                  category_name: category[:name].blank? ?
+                  Spree.t(:default_category_name) :
+                  category[:name],
+                  property_name: pp[:key],
+                  value: pp[:value]
+                )
+              end
             end
           end
 
