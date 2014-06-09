755d3e7003220f835448395bc7edb8d81a2db700
Richard Wilson <richard@freerunningtech.com>
2014-06-09 09:24:01 -0700

    Move uncategorized_properties scope to a better location.
    In an effort to try and dry up the SQL.

--- app/models/spree/property_decorator.rb
+++ app/models/spree/property_decorator.rb
@@ -1,14 +1,6 @@
 Spree::Property.class_eval do
-  scope :uncategorized_for_products, ->(product_ids) do
-    joins(:products).
-      includes(product_properties: :property_category).
-      where(spree_property_categories: { id: nil }).
-      where(spree_products: { id: product_ids }).
-      uniq
-  end
-
   scope :for_products, ->(product_ids) do
-    joins(product_properties: :product).
+    joins(:products).
       where(spree_products: { id: product_ids })
   end
 end
